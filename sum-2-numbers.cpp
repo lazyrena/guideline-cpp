0419e034a2f1c1db182d6c4565697ea6de546d55
lazyrena <161885085+lazyrena@users.noreply.github.com>
2024-03-10 01:55:32 +0700

    Create sum-2-numbers.cpp

---
+++ sum-2-numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  int firstNumber, secondNumber, res;
+  std::cin >> firstNumber >> secondNumber;
+  res = firstNumber + secondNumber;
+  std::cout << "Sum of 2 numbers is: " << res << std::endl;
+  return 0;
+}
